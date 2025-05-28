b1605e28df5da9ef8198083a1fbf3872039d6f52
ericyuyzr <zhenrui.yu@bccancer.bc.ca>
2025-05-28 09:49:54 -0700

    setup pacakegs for training

--- src/data_preprocessing_01.py
+++ src/data_preprocessing_01.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# Load your CSV file
 df = pd.read_csv('data/diagnosis_procedure_list.csv')  # Replace with your actual file path
 df = df.rename(columns={'site': 'target'})
 
-# Prepare empty dataframes for train and test sets
+
 train_df = pd.DataFrame()
 test_df = pd.DataFrame()
 
@@ -15,7 +14,7 @@ for site in df['target'].unique():
     site_train, site_test = train_test_split(
         site_data, 
         test_size=0.2, 
-        random_state=42,  # for reproducibility
+        random_state=42, 
         shuffle=True
     )
     train_df = pd.concat([train_df, site_train], ignore_index=True)
@@ -25,6 +24,5 @@ for site in df['target'].unique():
 train_df = train_df.sample(frac=1, random_state=42).reset_index(drop=True)
 test_df = test_df.sample(frac=1, random_state=42).reset_index(drop=True)
 
-# Save to new CSVs
 train_df.to_csv('data/train_data_01.csv', index=False)
 test_df.to_csv('data/test_data_01.csv', index=False)
